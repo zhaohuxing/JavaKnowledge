6093a3804aea48e20fd22a28318229df79056a57
x_zhaohu <x_zhaohu@163.com>
2017-01-27 01:00:45 +0800

    Javaintern

---
+++ intern/java/面试常考知识点/BasicType.java
@@ -0,0 +1,11 @@
+public class BasicType {
+	public static void main(String[] args) {
+		System.out.println(Byte.SIZE/8);
+		System.out.println(Short.SIZE/8);
+		System.out.println(Integer.SIZE/8);
+		System.out.println(Float.SIZE/8);
+		System.out.println(Double.SIZE/8);
+		System.out.println(Long.SIZE/8);
+		System.out.println(Character.SIZE/8);
+	}
+}
