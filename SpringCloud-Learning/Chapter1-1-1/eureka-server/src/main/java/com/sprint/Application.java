ad8e92020ad1f63315f42f793859970e95c8a74d
x_zhaohu <x_zhaohu@163.com>
2017-02-25 19:59:14 +0800

    cloud update readme

--- SpringCloud-Learning/Chapter1-1-1/eureka-server/src/main/java/com/sprint/Application.java
+++ SpringCloud-Learning/Chapter1-1-1/eureka-server/src/main/java/com/sprint/Application.java
@@ -2,12 +2,14 @@ package com.sprint;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		//new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ SpringCloud-Learning/readme.md
@@ -0,0 +1,1 @@
+学习资源来自于DiDi大神，为了保证版权所有，附上源链接http://git.oschina.net/didispace/SpringCloud-Learning/tree/master/
