a6bb77fb1e617743eca8c35d219426e0c11127eb
x_zhaohu <x_zhaohu@163.com>
2017-03-30 17:08:09 +0800

    update without type

--- JavaSE/MyJDBC/src/main/java/com/sprint/util/DBReflectionUtil.java
+++ JavaSE/MyJDBC/src/main/java/com/sprint/util/DBReflectionUtil.java
@@ -19,13 +19,29 @@ import java.util.Set;
  */
 public class DBReflectionUtil {
 
+	
+	/**
+	 * 检查数据是否合法
+	 * @param tableName
+	 * @param object 实体类
+	 */
 	private static <T> boolean isEnabled(String tableName, T object) {
-		if (tableName.trim().equals("") || tableName == null || object == null)
+		if (tableName.trim().equals("") 
+						|| tableName == null 
+							|| object == null)
 			return false;
 		return true;
 	}
 
-	private static <T> boolean isEnabled(String tableName, T object, Map<String, Object> map, String operator1) {
+	/**
+	 * 检查数据是否合法
+	 * @param tableName
+	 * @param object 实体类
+	 * @param map 数据库中字段与值 
+	 * @param operator1 "=" or "like"
+	 */
+	private static <T> boolean isEnabled(String tableName, T object,
+							Map<String, Object> map, String operator1) {
 		if (!isEnabled(tableName, object))
 			return false;
 		if (map.size() == 0 || operator1.trim().equals("") || operator1 == null)
@@ -33,13 +49,177 @@ public class DBReflectionUtil {
 		return true;
 	}
 
-	private static <T> boolean isEnabled(String tableName, T object, Map<String, Object> map, String operator1, String operator2) {
+	/**
+	 * 检查数据是否合法
+	 * @param tableName
+	 * @param object 实体类
+	 * @param map 数据库中字段与值 
+	 * @param operator1 "=" or "like"
+	 * @param operator2 "and" or "or"
+	 */
+	private static <T> boolean isEnabled(String tableName, T object,
+							Map<String, Object> map, String operator1, String operator2) {
 		if (!isEnabled(tableName, object, map, operator1))
 			return false;
 		if (operator2.trim().equals("") || operator2 == null)
 			return false;
 		return true;
 	}
+
+	/**
+	 * map中存放字段和值，operator1 代表操作符，operator2 代表运算符
+	 * 该方法的作用就是将其组合起来, 栗子如下： 
+	 * 	假设 map.put("name", "昵称"), map.put("age", 18); operator1值为"=", operator2值为"and"
+	 * 	则该方法返回： " where name = '昵称' and age = 18"
+	 *  @param map where后的条件中的键值对
+	 * 	@param operator1 "=" or "like" 
+	 *  @param operator2 "and" or "or"
+	 *  @return
+	 */
+	private static String getPartSql(Map<String, Object> map, String operator1, String operator2) {
+		StringBuffer sb = new StringBuffer();
+		Set<String> keys = map.keySet();
+		int index = 0;
+		for (String key : keys) {
+				sb.append(key).append(" " + operator1 + " ");
+				System.out.println(key + " :" + map.get(key).getClass());
+				if (map.get(key).getClass() == String.class) {
+					sb.append("\'")
+					  .append(map.get(key))
+					  .append("\'");
+				} else {
+					sb.append(map.get(key));
+				}
+				if (index != (keys.size() - 1)) {
+					sb.append(" " + operator2 + " ");
+				}
+				index++;
+		}
+		return sb.toString();
+	}
+
+
+	/**
+	 * 通过key更新一条或多条记录，key唯一
+	 * @param tableName
+	 * @param updataMap
+	 * @param optionMap
+	 * @param operator1
+	 */
+	public static boolean updateByKey(String tableName, Map<String, Object> updateMap,
+									Map<String, Object> optionMap, String operator1) {
+		return update(tableName, updateMap, optionMap, operator1, null);
+	} 
+	/**
+	 * 通过Keys更新一条或多条记录
+	 * @param tableName
+	 * @param updateMap
+	 * @param optionMap
+	 * @param operator1
+	 * @param operator2
+	 */
+	public static boolean updateByKeys(String tableName, Map<String, Object> updateMap,
+							Map<String, Object> optionMap, String operator1, String operator2) {
+		return update(tableName, updateMap, optionMap, operator1, operator2);
+	}
+
+	/**
+	 * 无条件修改全部记录
+	 * @param tableName
+	 * @param updateaMap
+	 */
+	public static boolean updateAll(String tableName, Map<String, Object> updateMap) {
+		return update(tableName, updateMap, null, null, null);
+	}
+
+	/**
+	 * 通过条件修改全部记录
+	 * @param tableName
+	 * @param updateMap
+	 * @param optionMap
+	 * @param operator1
+	 * @param operator2
+	 */
+	private static boolean update(String tableName, Map<String, Object> updateMap,
+						Map<String, Object> optionMap, String operator1, String operator2) {
+		StringBuffer sb = new StringBuffer("update ")
+											.append(tableName)
+											.append(" set ");
+		sb.append(getPartSql(updateMap, "=", ","));
+		if (optionMap != null) {
+			sb.append(" where ")
+			  .append(getPartSql(optionMap, operator1, operator2));
+		}
+		String sql = sb.toString();
+		System.out.println(sql);
+		Connection conn = ConnectionUtil.getConnection();	
+		PreparedStatement pstmt = null;
+		int updateRow = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			updateRow = pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if (updateRow != 0) 
+			return true;
+		return false;	
+	} 
+	/**
+	 * 通过key删除一条或多条记录。key通常是公共性的，比如:性别，学校
+	 * @param tableName
+	 * @param map where后的条件中的键值对
+	 * @param operator1 "=" or "like" 
+	 */
+	public static boolean deleteByKey(String tableName,
+							Map<String, Object> map, String operator1) {
+		return deleteByKeys(tableName, map, operator1, null);
+	}
+
+	/**
+	 * 通过keys删除一条或多条记录。key不能重复，唯一。
+	 * @param tableName
+	 * @param map where后的条件中的键值对
+	 * @param operator1 "=" or "like" 
+	 * @param operator2 "and" or "or"
+	 */ 
+	public static boolean deleteByKeys(String tableName, Map<String, Object> map,
+										String operator1, String operator2) {
+		StringBuffer sb = new StringBuffer(tableName).append(" where ")
+							.append(getPartSql(map, operator1, operator2));
+		return deleteAll(sb.toString());
+			
+	}
+
+	/**
+	 * 清空表中数据
+	 * @param tableName
+	 */
+	public static boolean deleteAll(String tableName) {
+		StringBuffer sb = new StringBuffer("delete from ")
+										.append(tableName);
+		Connection conn = ConnectionUtil.getConnection();
+		String sql = sb.toString();
+		System.out.println(sql);
+		PreparedStatement pstmt = null;
+		int rowNum = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rowNum = pstmt.executeUpdate();
+			System.out.println(rowNum);
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		if (rowNum != 0)
+			return true;
+		return false;
+	}
+
 	/**
 	 * 通过key获取单条记录, key值要求唯一, 例如：身份证号，唯一的
 	 * @param tableName 
@@ -48,12 +228,13 @@ public class DBReflectionUtil {
 	 * @param operator1 "=" or "like"
 	 * @return 
 	 */
-	public static <T> T findOnlyByKey(String tableName, T object, Map<String, Object> map, String operator1) {
+	public static <T> T findOnlyByKey(String tableName, T object,
+							Map<String, Object> map, String operator1) {
 		if (!isEnabled(tableName, object, map, operator1))
 			throw new IllegalArgumentException("参数不合法"); 
-		List<T> list = findMoreByKey(tableName, object, map, operator1);
-		if (list.size() != 0) {
-			return list.get(0);
+		T t = findOnlyByKeys(tableName, object, map, operator1, null);
+		if (t != null) { //flag
+			return t;
 		}
 		return null;
 	}
@@ -66,24 +247,35 @@ public class DBReflectionUtil {
 	 * @param operator1 "=" or "like"
 	 * @return 
 	 */
-	public static <T> List<T> findMoreByKey(String tableName, T object, Map<String, Object> map, String operator1) {
+	public static <T> List<T> findMoreByKey(String tableName, T object, 
+								  Map<String, Object> map, String operator1) {
 		if (!isEnabled(tableName, object, map, operator1))
 			throw new IllegalArgumentException("参数不合法"); 
 		return findMoreByKeys(tableName, object, map, operator1, null); 
 	}
 
 
+	/**
+	 * 通过keys获取单条记录，key值可重复，但key唯一
+	 * @param tableName
+	 * @param object 实体类，仅实例化
+	 * @param map 数据库中字段与值
+	 * @param operator1 "=" or "like"
+	 * @param operator2 "and" or "or"
+	 * @return 
+	 */
 	public static <T> T findOnlyByKeys(String tableName, T object,
 						Map<String, Object> map, String operator1, String operator2) {
 		if (!isEnabled(tableName, object, map, operator1))
 			throw new IllegalArgumentException("参数不合法"); 
 		List<T> list = findMoreByKeys(tableName, object, map, operator1, operator2);
-		if (list.size() != 0) {
+		if (list != null && list.size() != 0) {
 			return list.get(0);
 		}
 		return null;
 		
 	}
+
 	/**
 	 * 通过keys获取多条记录, key值可重复，但key唯一
 	 * @param tableName
@@ -93,24 +285,12 @@ public class DBReflectionUtil {
 	 * @param operator2 "and" or "or"
 	 * @return 
 	 */
-	public static <T> List<T> findMoreByKeys(String tableName, T object, Map<String, Object> map, String operator1, String operator2) {
+	public static <T> List<T> findMoreByKeys(String tableName, T object,
+						Map<String, Object> map, String operator1, String operator2) {
 		if (!isEnabled(tableName, object, map, operator1))
 			throw new IllegalArgumentException("参数不合法"); 
-		Set<String> keys = map.keySet();
 		StringBuffer sb = new StringBuffer(tableName).append(" where ");
-		int index = 0;
-		for (String key : keys) {
-				sb.append(key).append(" " + operator1 + " ");
-				if (key.getClass() == String.class) {
-					sb.append("\'")
-					  .append(map.get(key))
-					  .append("\'");
-				}
-				if (index != (keys.size() - 1)) {
-					sb.append(" " + operator2 + " ");
-				}
-				index++;
-		}
+			sb.append(getPartSql(map, operator1, operator2));
 		return findAll(sb.toString(), object);
 	}
 
@@ -151,6 +331,9 @@ public class DBReflectionUtil {
 						Method m = getMethod(object.getClass(), "set", key);
 						m.invoke(obj, rs.getLong(key));
 					} else if (paramsType.get(key) == boolean.class) {
+						Method m = getMethod(object.getClass(), "set", key);
+						m.invoke(obj, rs.getBoolean(key));
+					} else if (paramsType.get(key) == Date.class) {
 						Method m = getMethod(object.getClass(), "set", key);
 						m.invoke(obj, rs.getBoolean(key));
 					}
@@ -251,6 +434,9 @@ public class DBReflectionUtil {
 				} else if (paramsType.get(key) == boolean.class) {
 					Method m = getMethod(object.getClass(), "get", key);
 					pstmt.setBoolean(i, (Boolean)m.invoke(object));
+				} else if (paramsType.get(key) == Date.class) {
+					Method m = getMethod(object.getClass(), "get", key);
+					pstmt.setBoolean(i, (Date)m.invoke(object));
 				}
 			}
 			pstmt.executeUpdate();
